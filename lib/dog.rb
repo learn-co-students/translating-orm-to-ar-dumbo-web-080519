a509e900ed0eebbaef13051567f5f5cfb53c2810
Brian Wong <bwong92@gmail.com>
2019-08-16 09:21:26 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,2 +1,11 @@
-class Dog
+class Dog < ActiveRecord::Base
+
+    # attr_accessor :name, :breed 
+
+    # def initialize(breed_hash)
+    #     breed_hash.each do |attribute,value|
+    #         self.send("#{attribute}=",value)
+    #     end
+    # end
 end
+
